27ea8aec37e706f8225c7dc25e527392bf4edad4
J <J@afshans-MacBook-Pro.local>
2020-10-10 23:25:46 -0700

    first commit

---
+++ livetest1/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World</h1>
+        <p>Easy to use GitHub pages</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
